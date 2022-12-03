5798461c6a69a0a35dc31a1a6574aa49b4655996
AnderGI <agibarguen@gmail.com>
2022-12-03 13:50:45 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import _ from 'lodash';
 import './style.css';
 
- function component() {
-   const element = document.createElement('div');
+(function(){
 
-  
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-   return element;
- }
-
- document.body.appendChild(component());
+})();
