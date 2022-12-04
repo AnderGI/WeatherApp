6b97b9bc1637c2b4cb4d2a255747a790aee57651
AnderGI <agibarguen@gmail.com>
2022-12-04 12:50:16 +0100

    Styling and layout for the daily forecast

--- src/displayDailyForecastInfo.js
+++ src/displayDailyForecastInfo.js
@@ -2,23 +2,28 @@ import format from "date-fns/format";
 import { es } from "date-fns/locale";
 
 export function displayDayWeatherInfo(b) {
-  const nameAndDate = document.querySelector("[data-forecast='nameAndDate']");
-  const dayForecast = document.querySelector("[data-forecast='day']");
-  const imgAndTemp = document.querySelector("[data-forecast='imgAndTemp']");
+
+  //UNA FUNCION
+  const nameAndDate = document.querySelector("#nameAndDate");
+  const dayForecast = document.querySelector("#day");
+  dayForecast.innerHTML = "";
+  const imgAndTemp = document.querySelector("#imgAndTemp");
 
   let fecha = new Date();
   const nameP = document.createElement("p");
   nameP.innerHTML =
     b["city"]["name"] +
+    ", " +
+    format(fecha, "p", { locale: es }) +
     "<br />" +
-    format(fecha, "PPPP", { locale: es }) +
-    " " +
-    format(fecha, "p", { locale: es });
+    format(fecha, "PPPP", { locale: es }) 
+    ;
 
+  nameAndDate.innerHTML = "";
+  nameAndDate.append(nameP);
 
-    nameAndDate.append(nameP);
-  
-  //FORECAST FOR THE COMPLETE DAY
+  //FORECAST FOR THE COMPLETE DAY  
+  //ASYNCRONO EN OTRA FUNCION
   for (let weatherInfo of b["list"]) {
     if (
       //si la fecha de hoy es la misma que la que la api te da 12-05-2022 si es igual 12-05-2022
@@ -30,59 +35,83 @@ export function displayDayWeatherInfo(b) {
         )
       ).toDateString()
     ) {
-      const container = document.createElement("div");
-      const p1 = document.createElement("p");
-      const p2 = document.createElement("p");
-      p1.textContent = weatherInfo["dt_txt"].substring(
-        0,
-        weatherInfo["dt_txt"].lastIndexOf(" ")
-      );
+      const containerDateAndHourOfDayForecast = document.createElement("div");
+      containerDateAndHourOfDayForecast.setAttribute("class","dateAndHourDayForecast")
+      const containerImageOfDateAndhourDayForecast = document.createElement("div")
+      const dayAndDateP = document.createElement("p");
 
-      container.append(p1);
+      dayAndDateP.textContent =
+        format(
+          Date.parse(
+            weatherInfo["dt_txt"].substring(
+              0,
+              weatherInfo["dt_txt"].lastIndexOf(" ")
+            )
+          ),
+          "PPPP",
+          { locale: es }
+        ) +
+        " a las " + ". " +
+  
+            weatherInfo["dt_txt"].substring(
+              weatherInfo["dt_txt"].lastIndexOf(" ") + 1
+            ) + weatherInfo["weather"][0]["description"]
+      ;
+              
+      
 
-      p2.textContent = weatherInfo["dt_txt"].substring(
-        weatherInfo["dt_txt"].lastIndexOf(" ") + 1
-      );
 
-      container.append(p2);
 
-      dayForecast.append(container);
+        containerDateAndHourOfDayForecast.append(dayAndDateP)
+
+        containerImageOfDateAndhourDayForecast.style.cssText = ` 
+        background-image: url(http://openweathermap.org/img/wn/${weatherInfo['weather'][0]['icon']}@2x.png);
+        background-repeat: no-repeat;
+        background-size: contain;
+        background-position: center;
+      
+      `;
+
+    
+      console.log(b)
+      dayForecast.append(containerImageOfDateAndhourDayForecast)
+      dayForecast.append(containerDateAndHourOfDayForecast);
     }
   }
 
   //Img and temp
   //funcion que recibe un gif de giphy
-
+  //UNA FUNCION
   let container1 = document.createElement("div");
   container1.setAttribute("id", "weatherGif");
+
   let container2 = document.createElement("div");
+  container2.setAttribute("id", "weatherTemp");
   let pTemp = document.createElement("p");
-  pTemp.setAttribute("id","temp")
-  pTemp.textContent = b["list"][0]["main"]["temp"];
+  pTemp.setAttribute("id", "temp");
+  pTemp.textContent = b["list"][0]["main"]["temp"] + " K";
   container2.append(pTemp);
 
+  imgAndTemp.innerHTML = "";
   imgAndTemp.append(container1);
   imgAndTemp.append(container2);
 
-   // ESTO EN DESCRIPCION
-  
-  const description = document.querySelector("[data-forecast='description']");
-  const pDescription = document.createElement('div');
+  // ESTO EN DESCRIPCION
+  //OTRA FUNCION
+  const description = document.querySelector("#description");
+  const pDescription = document.createElement("div");
   pDescription.innerHTML = `
   <ul>
     <li>${b["list"][0]["weather"][0]["description"]}</li>
     <li>Humidity : ${b["list"][0]["main"]["humidity"]} %</li>
     <li>Wind speed : ${b["list"][0]["wind"]["speed"]} m/s</li>
   </ul>
-  `
-  
-  
-  
-  
-  description.append(pDescription);
+  `;
 
-  const pHumidity = document.createElement('p')
+  description.innerHTML = "";
+  description.appendChild(pDescription);
 
+  const pHumidity = document.createElement("p");
 
   console.log(b);
 }
--- src/index.js
+++ src/index.js
@@ -28,10 +28,27 @@ import { displayDayWeatherInfo } from "./displayDailyForecastInfo";
     try {
       const promiseObj = await fetchCountry(searchInputValue, "ESP");
       const promiseUrl = forecast(promiseObj["Lat"], promiseObj["Lon"]);
-      const fetchUrlPromise = await fetch(promiseUrl);
+      const fetchUrlPromise = await fetch(promiseUrl, {
+        mode:"cors"
+      });
       const jsonURLPromise = await fetchUrlPromise.json();
       displayDayWeatherInfo(jsonURLPromise);
-      fetchGIF(jsonURLPromise["list"][0]["weather"][0]["main"]);
+      console.log(jsonURLPromise["list"][0]["weather"][0]["icon"])
+      
+      const weatherGif = document.getElementById("weatherGif");
+      //get de icong depening on the weather from openweathermap
+      weatherGif.style.cssText = ` 
+        background-image: url(http://openweathermap.org/img/wn/${jsonURLPromise["list"][0]["weather"][0]["icon"]}@2x.png);
+        background-repeat: no-repeat;
+        background-size: contain;
+        background-position: center;
+      
+      `;
+      
+      
+      
+      console.log("http://openweathermap.org/img/wn/" + jsonURLPromise["list"][0]["weather"][0]["icon"]+ "@2x.png")
+      //fetchGIF(jsonURLPromise["list"][0]["weather"][0]["main"]);
       /**
        * Para la foto
        * ["data"][0]["images"]["original"]["url"]
@@ -42,7 +59,9 @@ import { displayDayWeatherInfo } from "./displayDailyForecastInfo";
   }
 
   async function fetchCountry(country, state) {
-    const request = await fetch(geocording(country, state));
+    const request = await fetch(geocording(country, state),{
+      mode:"cors",
+    });
     const response = await request.json();
     return {
       Lat: response[0]["lat"],
@@ -50,30 +69,4 @@ import { displayDayWeatherInfo } from "./displayDailyForecastInfo";
     };
   }
 
-  async function fetchGIF(w) {
-    /**
-     * https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=m8EuLgASSwTOUTEQBCpibavt4qDnKPun&limit=5
-     */
-    const part1 = "https://api.giphy.com/v1/gifs/search?q=";
-    const word = w;
-    const part2 = "&api_key=m8EuLgASSwTOUTEQBCpibavt4qDnKPun&limit=5";
-
-    const response = await fetch(part1 + word + part2);
-    const json = await response.json();
-
-    ///////
-
-    const weatherGif = document.getElementById("weatherGif");
-
-    weatherGif.style.cssText = ` 
-      background-image: url(${json["data"][0]["images"]["original"]["url"]});
-      background-repeat: no-repeat;
-      background-size: contain;
-      background-position: center;
-    
-    `;
-
-    /////
-    console.log(json);
-  }
 })();
