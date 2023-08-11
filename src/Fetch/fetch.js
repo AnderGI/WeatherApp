5b96eabf9ad2c5cb8bff0a50698ac0b8710b8694
AnderGI <agibarguen@gmail.com>
2023-08-11 17:37:26 +0200

    Cors mode added

--- src/Fetch/fetch.js
+++ src/Fetch/fetch.js
@@ -2,7 +2,10 @@
 export function weatherForecast(cityName, key) {
   return new Promise((resolve, reject) => {
     fetch(
-      `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}`
+      `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${key}`,
+      {
+        mode: "cors",
+      }
     )
       .then((res) => res.json())
       .then((data) => resolve(data))
